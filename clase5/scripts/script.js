fed9f164ea1e46e97a92932a91569cfacd84e284
ValentinZoia <90575422+ValentinZoia@users.noreply.github.com>
2021-11-17 23:43:58 -0300

    Delete clase5 directory

--- clase5/scripts/script.js
+++
@@ -1,9 +0,0 @@
-let brg = document.querySelector(".burger");
-let nav = document.querySelector(".container-2-nav-bar")
-
-brg.addEventListener('click',()=>{
-    brg.classList.toggle('brg-active');
-    nav.classList.toggle('nav-active');
-})
-
-console.log("nashe")
